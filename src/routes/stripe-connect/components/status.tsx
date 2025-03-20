e8ee16e70659b598801044f9d2fe8fc0ffb39653
pfulara <fulara.pawel@gmail.com>
2025-03-20 14:23:43 +0100

    feat/stripe-connect

---
+++ src/routes/stripe-connect/components/status.tsx
@@ -0,0 +1,30 @@
+import { StatusCell as StatusCell_ } from '../../../components/table/table-cells/common/status-cell';
+
+type StatusCellProps = {
+  status: 'pending' | 'connected' | 'not connected';
+};
+
+const getStatusColor: any = (status: string) => {
+  switch (status) {
+    case 'pending':
+      return 'orange';
+    case 'connected':
+      return 'green';
+    case 'not connected':
+      return 'red';
+    default:
+      return 'grey';
+  }
+};
+
+export const Status = ({ status }: StatusCellProps) => {
+  return (
+    <div className='flex h-full w-full items-center overflow-hidden'>
+      <span className='truncate capitalize'>
+        <StatusCell_ color={getStatusColor(status)}>
+          {status}
+        </StatusCell_>
+      </span>
+    </div>
+  );
+};
