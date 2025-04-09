9ff9febf2430058f0409cfeb795683271fb209e3
pfulara <fulara.pawel@gmail.com>
2025-04-09 09:41:36 +0200

    feat/api-keys

--- src/routes/api-key-management/api-key-management-detail/loader.ts
+++ src/routes/api-key-management/api-key-management-detail/loader.ts
@@ -1,17 +1,20 @@
-import { LoaderFunctionArgs } from "react-router-dom"
+import { LoaderFunctionArgs } from 'react-router-dom';
 
-import { apiKeysQueryKeys } from "../../../hooks/api/api-keys"
-import { sdk } from "../../../lib/client"
-import { queryClient } from "../../../lib/query-client"
+import { apiKeysQueryKeys } from '../../../hooks/api/api-keys';
+import { fetchQuery } from '../../../lib/client';
+import { queryClient } from '../../../lib/query-client';
 
 const apiKeyDetailQuery = (id: string) => ({
   queryKey: apiKeysQueryKeys.detail(id),
-  queryFn: async () => sdk.admin.apiKey.retrieve(id),
-})
+  queryFn: async () =>
+    fetchQuery(`/vendor/api-keys/${id}`, { method: 'GET' }),
+});
 
-export const apiKeyLoader = async ({ params }: LoaderFunctionArgs) => {
-  const id = params.id
-  const query = apiKeyDetailQuery(id!)
+export const apiKeyLoader = async ({
+  params,
+}: LoaderFunctionArgs) => {
+  const id = params.id;
+  const query = apiKeyDetailQuery(id!);
 
-  return queryClient.ensureQueryData(query)
-}
+  return queryClient.ensureQueryData(query);
+};
