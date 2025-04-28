54d4b403b4157ed6b05824892627bc1180633766
pfulara <fulara.pawel@gmail.com>
2025-04-28 15:40:06 +0200

    product categories fix

--- src/routes/collections/collection-add-products/collection-add-products.tsx
+++ src/routes/collections/collection-add-products/collection-add-products.tsx
@@ -1,22 +1,25 @@
-import { useParams } from "react-router-dom"
+import { useParams } from 'react-router-dom';
 
-import { RouteFocusModal } from "../../../components/modals"
-import { useCollection } from "../../../hooks/api/collections"
-import { AddProductsToCollectionForm } from "./components/add-products-to-collection-form"
+import { RouteFocusModal } from '../../../components/modals';
+import { useCollection } from '../../../hooks/api/collections';
+import { AddProductsToCollectionForm } from './components/add-products-to-collection-form';
 
 export const CollectionAddProducts = () => {
-  const { id } = useParams()
-  const { collection, isLoading, isError, error } = useCollection(id!)
+  const { id } = useParams();
+  const { product_collection, isLoading, isError, error } =
+    useCollection(id!);
 
   if (isError) {
-    throw error
+    throw error;
   }
 
   return (
     <RouteFocusModal>
-      {!isLoading && collection && (
-        <AddProductsToCollectionForm collection={collection} />
+      {!isLoading && product_collection && (
+        <AddProductsToCollectionForm
+          collection={product_collection}
+        />
       )}
     </RouteFocusModal>
-  )
-}
+  );
+};
--- src/routes/products/product-organization/product-organization.tsx
+++ src/routes/products/product-organization/product-organization.tsx
@@ -1,32 +1,38 @@
-import { Heading } from "@medusajs/ui"
-import { useTranslation } from "react-i18next"
-import { useParams } from "react-router-dom"
+import { Heading } from '@medusajs/ui';
+import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
 
-import { RouteDrawer } from "../../../components/modals"
-import { useProduct } from "../../../hooks/api/products"
-import { PRODUCT_DETAIL_FIELDS } from "../product-detail/constants"
-import { ProductOrganizationForm } from "./components/product-organization-form"
+import { RouteDrawer } from '../../../components/modals';
+import { useProduct } from '../../../hooks/api/products';
+import { ProductOrganizationForm } from './components/product-organization-form';
 
 export const ProductOrganization = () => {
-  const { id } = useParams()
-  const { t } = useTranslation()
+  const { id } = useParams();
+  const { t } = useTranslation();
 
-  const { product, isLoading, isError, error } = useProduct(id!, {
-    fields: PRODUCT_DETAIL_FIELDS,
-  })
+  const { product, isLoading, isError, error } = useProduct(
+    id!,
+    {
+      fields: '*categories',
+    }
+  );
 
   if (isError) {
-    throw error
+    throw error;
   }
 
   return (
     <RouteDrawer>
       <RouteDrawer.Header>
         <RouteDrawer.Title asChild>
-          <Heading>{t("products.organization.edit.header")}</Heading>
+          <Heading>
+            {t('products.organization.edit.header')}
+          </Heading>
         </RouteDrawer.Title>
       </RouteDrawer.Header>
-      {!isLoading && product && <ProductOrganizationForm product={product} />}
+      {!isLoading && product && (
+        <ProductOrganizationForm product={product} />
+      )}
     </RouteDrawer>
-  )
-}
+  );
+};
