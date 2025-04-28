6259484340b23be06c2cf6fff219aa2bc6a275c0
pfulara <fulara.pawel@gmail.com>
2025-04-28 13:20:17 +0200

    collection / product types / tags adding

--- src/routes/product-types/product-type-create/components/create-product-type-form/create-product-type-form.tsx
+++ src/routes/product-types/product-type-create/components/create-product-type-form/create-product-type-form.tsx
@@ -1,97 +1,112 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Button, Heading, Input, Text, toast } from "@medusajs/ui"
-import { useForm } from "react-hook-form"
-import { useTranslation } from "react-i18next"
-import { z } from "zod"
-import { Form } from "../../../../../components/common/form"
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Button,
+  Heading,
+  Input,
+  Text,
+  toast,
+} from '@medusajs/ui';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+import { Form } from '../../../../../components/common/form';
 import {
   RouteFocusModal,
   useRouteModal,
-} from "../../../../../components/modals"
-import { KeyboundForm } from "../../../../../components/utilities/keybound-form"
-import { useCreateProductType } from "../../../../../hooks/api/product-types"
+} from '../../../../../components/modals';
+import { KeyboundForm } from '../../../../../components/utilities/keybound-form';
+import { useCreateProductType } from '../../../../../hooks/api/product-types';
 
 const CreateProductTypeSchema = z.object({
   value: z.string().min(1),
-})
+});
 
 export const CreateProductTypeForm = () => {
-  const { t } = useTranslation()
-  const { handleSuccess } = useRouteModal()
+  const { t } = useTranslation();
+  const { handleSuccess } = useRouteModal();
 
-  const form = useForm<z.infer<typeof CreateProductTypeSchema>>({
+  const form = useForm<
+    z.infer<typeof CreateProductTypeSchema>
+  >({
     defaultValues: {
-      value: "",
+      value: '',
     },
     resolver: zodResolver(CreateProductTypeSchema),
-  })
+  });
 
-  const { mutateAsync, isPending } = useCreateProductType()
+  const { mutateAsync, isPending } = useCreateProductType();
 
   const handleSubmit = form.handleSubmit(
-    async (values: z.infer<typeof CreateProductTypeSchema>) => {
+    async (
+      values: z.infer<typeof CreateProductTypeSchema>
+    ) => {
       await mutateAsync(values, {
-        onSuccess: ({ product_type }) => {
+        onSuccess: () => {
           toast.success(
-            t("productTypes.create.successToast", {
-              value: product_type.value,
-            })
-          )
+            'Product type requested successfully'
+          );
 
-          handleSuccess(`/settings/product-types/${product_type.id}`)
+          handleSuccess();
         },
         onError: (e) => {
-          toast.error(e.message)
+          toast.error(e.message);
         },
-      })
+      });
     }
-  )
+  );
 
   return (
     <RouteFocusModal.Form form={form}>
       <KeyboundForm
         onSubmit={handleSubmit}
-        className="flex flex-col overflow-hidden"
+        className='flex flex-col overflow-hidden'
       >
         <RouteFocusModal.Header>
-          <div className="flex items-center justify-end gap-x-2">
+          <div className='flex items-center justify-end gap-x-2'>
             <RouteFocusModal.Close asChild>
-              <Button size="small" variant="secondary">
-                {t("actions.cancel")}
+              <Button size='small' variant='secondary'>
+                {t('actions.cancel')}
               </Button>
             </RouteFocusModal.Close>
             <Button
-              size="small"
-              variant="primary"
-              type="submit"
+              size='small'
+              variant='primary'
+              type='submit'
               isLoading={isPending}
             >
-              {t("actions.create")}
+              {t('actions.create')}
             </Button>
           </div>
         </RouteFocusModal.Header>
-        <RouteFocusModal.Body className="flex flex-col items-center overflow-y-auto p-16">
-          <div className="flex w-full max-w-[720px] flex-col gap-y-8">
+        <RouteFocusModal.Body className='flex flex-col items-center overflow-y-auto p-16'>
+          <div className='flex w-full max-w-[720px] flex-col gap-y-8'>
             <div>
-              <Heading>{t("productTypes.create.header")}</Heading>
-              <Text size="small" className="text-ui-fg-subtle">
-                {t("productTypes.create.hint")}
+              <Heading>
+                {t('productTypes.create.header')}
+              </Heading>
+              <Text
+                size='small'
+                className='text-ui-fg-subtle'
+              >
+                {t('productTypes.create.hint')}
               </Text>
             </div>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
               <Form.Field
                 control={form.control}
-                name="value"
+                name='value'
                 render={({ field }) => {
                   return (
                     <Form.Item>
-                      <Form.Label>{t("productTypes.fields.value")}</Form.Label>
+                      <Form.Label>
+                        {t('productTypes.fields.value')}
+                      </Form.Label>
                       <Form.Control>
                         <Input {...field} />
                       </Form.Control>
                       <Form.ErrorMessage />
                     </Form.Item>
-                  )
+                  );
                 }}
               />
             </div>
@@ -99,5 +114,5 @@ export const CreateProductTypeForm = () => {
         </RouteFocusModal.Body>
       </KeyboundForm>
     </RouteFocusModal.Form>
-  )
-}
+  );
+};
