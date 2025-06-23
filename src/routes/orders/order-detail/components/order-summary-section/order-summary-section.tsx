534250f986308168151c92df26addd36ee920f6d
pfulara <fulara.pawel@gmail.com>
2025-06-23 15:36:55 +0200

    vendor orders return hidden

--- src/routes/orders/order-detail/components/order-summary-section/order-summary-section.tsx
+++ src/routes/orders/order-detail/components/order-summary-section/order-summary-section.tsx
@@ -295,7 +295,7 @@ const Header = ({
   return (
     <div className="flex items-center justify-between px-6 py-4">
       <Heading level="h2">{t("fields.summary")}</Heading>
-      <ActionMenu
+      {/* <ActionMenu
         groups={[
           {
             actions: [
@@ -342,7 +342,7 @@ const Header = ({
             ],
           },
         ]}
-      />
+      /> */}
     </div>
   )
 }
