d00db57f616834cf1b8f3fd9ba00e17ba8f2192e
Paweł Fulara <39165521+pfulara@users.noreply.github.com>
2025-07-24 13:22:47 +0200

    fix medusa icons (#168)

--- src/routes/orders/order-receive-return/components/order-receive-return-form/order-receive-return-form.tsx
+++ src/routes/orders/order-receive-return/components/order-receive-return-form/order-receive-return-form.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod"
-import { ArrrowRight } from "@medusajs/icons"
+import { ArrowRight } from "@medusajs/icons"
 import { AdminOrder, AdminReturn } from "@medusajs/types"
 import { Alert, Button, Input, Switch, Text, toast } from "@medusajs/ui"
 import { useEffect, useMemo } from "react"
@@ -219,7 +219,7 @@ export function OrderReceiveReturnForm({
             <div>
               {stock_location && (
                 <div className="flex items-center gap-2">
-                  <ArrrowRight className="text-ui-fg-subtle" />{" "}
+                  <ArrowRight className="text-ui-fg-subtle" />{" "}
                   <span className="text-ui-fg-base txt-small font-medium">
                     {stock_location.name}
                   </span>
